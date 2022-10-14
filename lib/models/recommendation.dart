6a2e0938fc5237d730036adef90c0306cd9a8c96
Alfard <alfard.dev@gmail.com>
2022-10-14 23:08:12 +0200

    gitflow-feature-stash: recommandations_model_creation

---
+++ lib/models/recommendation.dart
@@ -0,0 +1,46 @@
+class Recommendation {
+  final String? name, entreprise, source, text, image;
+
+  const Recommendation({
+    this.name,
+    this.entreprise,
+    this.source,
+    this.text,
+    this.image,
+  });
+}
+
+final List<Recommendation> recommendations = [
+  const Recommendation(
+    name: "John Doe",
+    entreprise: "Entreprise",
+    source: "Source",
+    text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl.",
+    image: "https://i.imgur.com/2YQZQzS.png",
+  ),
+  const Recommendation(
+    name: "John Doe",
+    entreprise: "Entreprise",
+    source: "Source",
+    text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl.",
+    image: "https://i.imgur.com/2YQZQzS.png",
+  ),
+  const Recommendation(
+    name: "John Doe",
+    entreprise: "Entreprise",
+    source: "Source",
+    text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl.",
+    image: "https://i.imgur.com/2YQZQzS.png",
+  ),
+  const Recommendation(
+    name: "John Doe",
+    entreprise: "Entreprise",
+    source: "Source",
+    text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl. Sed euismod, nunc ut aliquam tincidunt, nunc nisl aliquam nisl, eget aliquam nunc nisl sit amet nisl.",
+    image: "https://i.imgur.com/2YQZQzS.png",
+  ),
+];
