6f2d7801f6d368db3bfc57b2cd9cdfe994816fac
Alfard <alfard.dev@gmail.com>
2022-10-14 23:03:12 +0200

    initialisation of the flutter web projet

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/constants/constants.dart';
+import 'package:portfolio/constants/text_constants.dart';
+import 'package:portfolio/screens/main_screen/components/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appName,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: bgColor,
+        canvasColor: bgColor,
+        textTheme: GoogleFonts.kanitTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white)
+            .copyWith(
+              bodyText1: const TextStyle(color: bodyTextColor),
+              bodyText2: const TextStyle(color: bodyTextColor),
+            ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
